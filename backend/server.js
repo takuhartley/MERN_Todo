c7158b3a58a9c3949b14e04eb5528242f15425d4
Robert Hartley <43013927+RobHart01@users.noreply.github.com>
2020-02-10 07:13:42 +0900

    Adding db logic

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,20 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
-const cors = require('cors')
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const PORT = 4000;
+const mongoose = require("mongoose");
 
 app.use(cors());
 app.use(bodyParser.json());
 
+mongoose.connect("mongodb://127.0.0.1:27017/todos", { useNewUrlParser: true });
+const connection = mongoose.connection;
+
+connection.once("open", function() {
+  console.log("MongoDB database connection is in fam");
+});
+
 app.listen(PORT, function() {
-    console.log("We're connected broski on Port: " + PORT);
-})
+  console.log("We're connected broski on Port: " + PORT);
+});
