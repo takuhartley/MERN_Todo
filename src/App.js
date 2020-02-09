f4ffbc2bf9d805a9ffeae2ac854001212ee0d99d
Robert Hartley <43013927+RobHart01@users.noreply.github.com>
2020-02-10 06:17:45 +0900

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+import "./App.css";
+import CreateTodo from "./Components/create-todo.component";
+import EditTodo from "./Components/edit-todo.component";
+import TodosList from "./Components/todos-list.component";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Route path="/" exact component={TodosList} />
+      <Route path="/edit/:id" component={EditTodo} />
+      <Route path="/create" component={CreateTodo} />
+
+      <div className="container">
+        <h1>Mern Stack Todo App</h1>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/Components/create-todo.component.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export class CreateTodo extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Todos List</h1>
+      </div>
+    );
+  }
+}
+
+export default CreateTodo;
