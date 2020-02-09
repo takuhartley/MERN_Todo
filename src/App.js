52f3c25d6a4c78b8f073008e5118ced4863083df
Robert Hartley <43013927+RobHart01@users.noreply.github.com>
2020-02-10 06:37:03 +0900

    Adding logic

--- src/App.js
+++ src/App.js
@@ -5,18 +5,32 @@ import "./App.css";
 import CreateTodo from "./Components/create-todo.component";
 import EditTodo from "./Components/edit-todo.component";
 import TodosList from "./Components/todos-list.component";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 
 function App() {
   return (
     <Router>
-      <Route path="/" exact component={TodosList} />
-      <Route path="/edit/:id" component={EditTodo} />
-      <Route path="/create" component={CreateTodo} />
-
-      <div className="container">
-        <h1>Mern Stack Todo App</h1>
-      </div>
-    </Router>
+        <div className="container">
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <Link to="/" className="navbar-brand">MERN-Stack Todo App</Link>
+            <div className="collpase navbar-collapse">
+              <ul className="navbar-nav mr-auto">
+                <li className="navbar-item">
+                  <Link to="/" className="nav-link">Todos</Link>
+                </li>
+                <li className="navbar-item">
+                  <Link to="/create" className="nav-link">Create Todo</Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
+          <br/>
+          <Route path="/" exact component={TodosList} />
+          <Route path="/edit/:id" component={EditTodo} />
+          <Route path="/create" component={CreateTodo} />
+        </div>
+      </Router>
   );
 }
 
--- src/Components/create-todo.component.js
+++ src/Components/create-todo.component.js
@@ -1,6 +1,48 @@
 import React, { Component } from "react";
 
 export class CreateTodo extends Component {
+  constructor(props) {
+    super(props);
+    this.onChangeTodoDescription = this.onChangeTodoDescription.bind(this);
+    this.onChangeTodoResponsible = this.onChangeTodoResponsible.bind(this);
+    this.onChangeTodoPriority = this.onChangeTodoPriority.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+    this.state = {
+      todo_description: "",
+      todo_responsible: "",
+      todo_priority: "",
+      todo_completed: false
+    };
+  }
+  onChangeTodoDescription(e) {
+    this.setState({
+      todo_description: e.target.value
+    });
+  }
+  onChangeTodoResponsible(e) {
+    this.setState({
+      todo_responsible: e.target.value
+    });
+  }
+  onChangeTodoPriority(e) {
+    this.setState({
+      todo_priority: e.target.value
+    });
+  }
+  onSubmit(e) {
+    e.preventDefault();
+    console.log(`Form submitted`);
+    console.log(`Todo Description: ${this.state.todo_description}`);
+    console.log(`Todo Responsible: ${this.state.todo_responsible}`);
+    console.log(`Todo Priority: ${this.state.todo_priority}`);
+
+    this.setState({
+      todo_description: "",
+      todo_responsible: "",
+      todo_priority: "",
+      todo_completed: false
+    });
+  }
   render() {
     return (
       <div>
